5b7ced6079bc456e4e20cc8f272e8349558f9503
bruno-brasil-babson <bbrasilmeinhart1@babson.edu>
2022-12-03 17:08:40 -0500

    .

--- main.py
+++ main.py
@@ -2,4 +2,8 @@
 
 import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
+from config import SPOTIPY_CLIENT_ID, SPOTIPY_CLIENT_SECRET
+
+spotify = spotipy.Spotify(client_credentials_manager=SpotifyClientCredentials(SPOTIPY_CLIENT_ID, SPOTIPY_CLIENT_SECRET))
+
 
