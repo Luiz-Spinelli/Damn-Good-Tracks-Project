ce2fb31f1485878200ecc61a35986bdd776c80fc
bruno-brasil-babson <bbrasilmeinhart1@babson.edu>
2022-12-04 17:47:10 -0500

    Update main.py

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
 from config import SPOTIPY_CLIENT_ID, SPOTIPY_CLIENT_SECRET
 from pprint import pprint
+import random
 
 spotify = spotipy.Spotify(client_credentials_manager=SpotifyClientCredentials(SPOTIPY_CLIENT_ID, SPOTIPY_CLIENT_SECRET))
 
@@ -34,14 +35,18 @@ def get_relatedArtists(artistID, num=10):
     """
     answers = spotify.artist_related_artists(artistID)
     related_artists = answers['artists']
+
     ids = []
 
     for i in range(num):
-        ids.append(related_artists[i]['id'])
+        if related_artists[i]['id'] != artistID:
+            ids.append(related_artists[i]['id'])
+        else:
+            num +=1
 
     return ids
 
-def rec_songs(list_artists_IDs):
+def rec_songs(list_artists_IDs, tracks_per_artist=2):
     """
     Input: list of related artits ID
     Recommendation algorithm (proposed by me & Bruno)
@@ -51,27 +56,45 @@ def rec_songs(list_artists_IDs):
         - randomize by picking 20 out of 50 songs 
     Output: list of track IDs
     """
-    tracks = []
+    tracks = {}
     for id in list_artists_IDs:
         output = spotify.artist_top_tracks(id)
         answers = output['tracks']
-        for t in answers:
-            tracks.append(t['name'])
+        sample_tracks = random.sample(answers, tracks_per_artist)
+
+        for t in sample_tracks:
+            track_id = t['id']
+            track_name = t['name']
+            track_artist = t['artists'][0]['name']
+            tracks[track_id] = {'name': track_name, 'artist': track_artist}
 
     return tracks
 
-def rec_albums(list_artists_IDs):
+def rec_albums(list_artists_IDs, albums_per_artist=1):
     """
     Input: list of related artits IDs
     Recommendation algorithm/logic = TBD
         - Pick 1 randomized album for each artist
-    Output: list of 10 final related 
+    Output: list of 10 final related albums
     """
+    albums = {}
+    for id in list_artists_IDs:
+        output = spotify.artist_albums(id, 'album', 'US')
+        answers = output['items']
+        sample_albums = random.sample(answers, albums_per_artist)
+
+        for a in sample_albums:
+            album_id = a['id']
+            album_name = a['name']
+            album_artist = a['artists'][0]['name']
+            albums[album_id] = {'name': album_name, 'artist': album_artist}
+        
+    return albums
 
 def main():
     id = get_artistID('Kanye West')
     related = get_relatedArtists(id)
-    pprint(rec_songs(related))
+    pprint(rec_albums(related))
 
 if __name__ == '__main__':
     main()
